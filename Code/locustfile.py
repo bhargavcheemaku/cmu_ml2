73aabba8e1fc45a291067f57652406ec16f94a99
bhargavcheemaku <scheemak@tepper.cmu.edu>
2024-11-21 09:29:51 -0500

    Updated new code

---
+++ Code/locustfile.py
@@ -0,0 +1,22 @@
+from locust import HttpUser, task, between
+
+class SimulateTourneyUser(HttpUser):
+    # Wait time between tasks (to mimic real user behavior)
+    wait_time = between(1, 5)
+    
+    @task
+    def simulate_tourney(self):
+        # Payload to be sent in the POST request
+        payload = {
+            "input": "1"  
+        }
+        
+        # Sending POST request to the /simulateTourney endpoint
+        response = self.client.post("/simulateTourney", json=payload)
+        
+        # Print the response or log for debugging
+        if response.status_code == 200:
+            print("Request successful:", response.json())
+        else:
+            print("Request failed with status code:", response.status_code)
+
